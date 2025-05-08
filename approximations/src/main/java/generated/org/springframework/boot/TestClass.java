d6e6f27dc2d213296c2f66bf0bb358f4dd1a6d45
MchKosticyn <mishakosticyn@yandex.ru>
2025-05-08 17:22:17 +0300

    [fix] updated spring test class

--- approximations/src/main/java/generated/org/springframework/boot/TestClass.java
+++ approximations/src/main/java/generated/org/springframework/boot/TestClass.java
@@ -2,18 +2,15 @@ package generated.org.springframework.boot;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest
 public class TestClass {
 
     @Autowired
     private MockMvc mockMvc;
 
     public static void ignoreResult(Object result) {
-
     }
 }
